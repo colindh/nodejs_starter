f86e5f1957eb09896939a582197b110b721e5990
Colin Hume <colindh@gmail.com>
2018-06-30 14:30:43 -0500

    set up dependencies and other improvements

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 var server = (function(){
-    var express = require('express'),
-        app = express();
+  var express = require('express'),
+  app = express();
 
-    app.use(express.static('public'));
+  app.use(express.static('public'));
 
-    // accept GET request
-    app.get('/', function (req, res) {
-	res.send('Got a GET request');
-    });
+  // accept GET request
+  app.get('/', function (req, res) {
+    res.send('Got a GET request');
+  });
 
-    // accept POST request
-    app.post('/', function (req, res) {
-	res.send('Got a POST request');
-    });
+  // accept POST request
+  app.post('/', function (req, res) {
+    res.send('Got a POST request');
+  });
 
-    // accept PUT request
-    app.put('/', function (req, res) {
-	res.send('Got a PUT request');
-    });
+  // accept PUT request
+  app.put('/', function (req, res) {
+    res.send('Got a PUT request');
+  });
 
-    // accept DELETE request
-    app.delete('/', function (req, res) {
-	res.send('Got a DELETE request');
-    });
+  // accept DELETE request
+  app.delete('/', function (req, res) {
+    res.send('Got a DELETE request');
+  });
 
 
 })();
